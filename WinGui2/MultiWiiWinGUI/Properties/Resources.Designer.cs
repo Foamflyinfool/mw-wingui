55e7e4440db0ee7c4caf5b9425cd0340ddc32416
eosbandi <eosbandi@gmail.com>
2013-04-01 18:21:59 +0000

    Mission planner is ready Go to Click is ready (using WP#16 at this moment) Some small code changes, and cleanup.
    Mission upload and download not implemented YET!

--- WinGui2/MultiWiiWinGUI/Properties/Resources.Designer.cs
+++ WinGui2/MultiWiiWinGUI/Properties/Resources.Designer.cs
@@ -203,6 +203,16 @@ namespace MultiWiiWinGUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap poshold_unlim {
+            get {
+                object obj = ResourceManager.GetObject("poshold_unlim", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap quadicon {
             get {
                 object obj = ResourceManager.GetObject("quadicon", resourceCulture);
